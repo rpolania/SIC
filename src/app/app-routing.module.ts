2860eff8b974e6f7ed2a4a555acef3fbbbe0b4bf
raul <raulpolania@gmail.com>
2021-01-20 14:27:28 -0500

    modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/form/form.component.css 	new file:   src/app/form/form.component.html 	new file:   src/app/form/form.component.spec.ts 	new file:   src/app/form/form.component.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'form', component:FormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onsubmit(_dato: any){
+    console.log(_dato.value);
+  }
+}
